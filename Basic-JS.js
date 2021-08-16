120ad82fc3821bb0e5ba99839524953ba1df5104
Prasetiyo <prassonly@gmail.com>
2021-08-16 22:10:38 +0700

    Update Basic-JS.js

--- Basic-JS.js
+++ Basic-JS.js
@@ -749,4 +749,55 @@ Passed
 Your code should use dot and bracket notation to access myPlants.
 */
 
-// Record Collection
+// Record Collection (hard)
+var recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+function updateRecords(records, id, prop, value) {
+  if (prop !== "tracks" && value !== "") {
+    records[id][prop] = value;
+  } else if (prop === "tracks" && records[id].hasOwnProperty("tracks") === false) {
+    records[id][prop] = [value];
+  } else if (prop === "tracks" && value !== "") {
+    records[id][prop].push(value);
+  } else if (value === "") {
+    delete records[id][prop];
+  }
+  return records;
+}
+
+/*
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+After updateRecords(recordCollection, 5439, "artist", "ABBA"), artist should be the string ABBA
+Passed
+After updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me"), tracks should have the string Take a Chance on Me as the last element.
+Passed
+After updateRecords(recordCollection, 2548, "artist", ""), artist should not be set
+Passed
+After updateRecords(recordCollection, 1245, "tracks", "Addicted to Love"), tracks should have the string Addicted to Love as the last element.
+Passed
+After updateRecords(recordCollection, 2468, "tracks", "Free"), tracks should have the string 1999 as the first element.
+Passed
+After updateRecords(recordCollection, 2548, "tracks", ""), tracks should not be set
+Passed
+After updateRecords(recordCollection, 1245, "albumTitle", "Riptide"), albumTitle should be the string Riptide
+*/
+
+// Iterate with JavaScript While Loops
