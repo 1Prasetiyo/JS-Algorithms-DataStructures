efe0f280e37f4f17165b2e6a351ebab1d41f338f
Prasetiyo <prassonly@gmail.com>
2021-08-18 11:23:50 +0700

    Update Basic-JS.js

--- Basic-JS.js
+++ Basic-JS.js
@@ -914,3 +914,24 @@ i should equal 11
 */
 
 // Replace Loops using Recursion
+function sum(arr, n) {
+  if (n <=0 ) {
+    return 0;
+  } else {
+    return sum(arr, n - 1) + arr[n -1];
+  }
+}
+
+/* Should :
+sum([1], 0) should equal 0.
+Passed
+sum([2, 3, 4], 1) should equal 2.
+Passed
+sum([2, 3, 4, 5], 3) should equal 9.
+Passed
+Your code should not rely on any kind of loops (for or while or higher order functions such as forEach, map, filter, or reduce.).
+Passed
+You should use recursion to solve this problem.
+*/
+
+// Profile Lookup
