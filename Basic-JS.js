259e30324a3c1e66bb7eb36bafe061419860f8df
Prasetiyo <prassonly@gmail.com>
2021-08-19 17:24:01 +0700

    Update Basic-JS.js

--- Basic-JS.js
+++ Basic-JS.js
@@ -1082,3 +1082,40 @@ checkSign(0) should return the string zero. Note that capitalization matters
 */
 
 // Use Recursion to Create a Countdown
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum - startNum === 0) {
+    return [startNum];
+  } else {
+    var numbers = rangeOfNumbers(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+  }
+}
+// Or
+function rangeOfNumbers(startNum, endNum) {
+  return startNum === endNum
+    ? [startNum]
+    : rangeOfNumbers(startNum, endNum - 1).concat(endNum);
+}
+// Or
+function rangeOfNumbers(startNum, endNum) {
+  return startNum === endNum
+    ? [startNum]
+    : [...rangeOfNumbers(startNum, endNum - 1), endNum ];
+}
+
+/* Should :
+Your function should return an array.
+Passed
+Your code should not use any loop syntax (for or while or higher order functions such as forEach, map, filter, or reduce).
+Passed
+rangeOfNumbers should use recursion (call itself) to solve this challenge.
+Passed
+rangeOfNumbers(1, 5) should return [1, 2, 3, 4, 5].
+Passed
+rangeOfNumbers(6, 9) should return [6, 7, 8, 9].
+Passed
+rangeOfNumbers(4, 4) should return [4].
+*/
+
+// END
