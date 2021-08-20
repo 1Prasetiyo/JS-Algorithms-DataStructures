0461f579993711dc51126123fe005c7dbe3d9db0
Prasetiyo <prassonly@gmail.com>
2021-08-20 13:46:42 +0700

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -19,3 +19,22 @@ quote should be the string Oliver says Meow!
 */
 
 // Compare Scopes of the var and let Keywords
+function checkScope() {
+  let i = 'function scope';
+  if (true) {
+    let i = 'block scope';
+      console.log('Block scope i is: ', i);
+  }
+  console.log('Function scope i is: ', i);
+  return i;
+}
+
+/* Should :
+var should not exist in code.
+Passed
+The variable i declared in the if statement should equal the string block scope.
+Passed
+checkScope() should return the string function scope
+*/
+
+// Declare a Read-Only Variable with the const Keyword
