62f052b1574b6dc77067d9a7c39d0e32b082af12
Prasetiyo <prassonly@gmail.com>
2021-08-20 12:29:27 +0700

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -1,1 +1,21 @@
+// Explore Differences Between the var and let Keywords
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
 
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
+
+/* Should :
+var should not exist in the code.
+Passed
+catName should be the string Oliver.
+Passed
+quote should be the string Oliver says Meow!
+*/
+
+// Compare Scopes of the var and let Keywords
