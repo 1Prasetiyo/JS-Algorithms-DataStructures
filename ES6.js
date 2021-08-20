181f0cfebc4aaf13ed5a07fa1bf61c2d4261021c
Prasetiyo <prassonly@gmail.com>
2021-08-20 23:19:20 +0700

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -77,3 +77,28 @@ s should be equal to [2, 5, 7].
 */
 
 // Prevent Object Mutation
+
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };  Object.freeze(MATH_CONSTANTS);
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch(ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+/* Should :
+You should not replace the const keyword.
+Passed
+MATH_CONSTANTS should be a constant variable (by using const).
+Passed
+You should not change the original declaration of MATH_CONSTANTS.
+Passed
+PI should equal 3.14.
+*/
+
+// Use Arrow Functions to Write Concise Anonymous Functions
