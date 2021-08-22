155d6575dabf7cfe0603533a3104cd8e1768c7b9
Prasetiyo <prassonly@gmail.com>
2021-08-23 01:44:35 +0700

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -301,3 +301,36 @@ Destructuring on list should be used.
 */
 
 // Use Destructuring Assignment to Pass an Object as a Function's Parameters
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+const half = (stats) => (stats.max + stats.min) / 2.0; 
+// return to
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+const half = ({max, min}) => (max + min) / 2.0; 
+
+/* Should :
+stats should be an object.
+Passed
+half(stats) should be 28.015
+Passed
+Destructuring should be used.
+Passed
+Destructured parameter should be used.
+*/
+
+// Create Strings using Template Literals
