08cadb9acbf387e27ac4e714d6c069ed7feb2e41
Prasetiyo <prassonly@gmail.com>
2021-08-25 10:06:05 +0700

    Update RegEx.js

--- RegEx.js
+++ RegEx.js
@@ -22,3 +22,51 @@ You should perform a literal string match with your regex.
 */
 
 // Match a Literal String with Different Possibilities
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
+
+/* Should :
+Your regex petRegex should return true for the string John has a pet dog.
+Passed
+Your regex petRegex should return false for the string Emma has a pet rock.
+Passed
+Your regex petRegex should return true for the string Emma has a pet bird.
+Passed
+Your regex petRegex should return true for the string Liz has a pet cat.
+Passed
+Your regex petRegex should return false for the string Kara has a pet dolphin.
+Passed
+Your regex petRegex should return true for the string Alice has a pet fish.
+Passed
+Your regex petRegex should return false for the string Jimmy has a pet computer.
+*/
+
+// Ignore Case While Matching
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i; // Change this line
+let result = fccRegex.test(myString);
+
+/* Should :
+Your regex should match the string freeCodeCamp
+Passed
+Your regex should match the string FreeCodeCamp
+Passed
+Your regex should match the string FreecodeCamp
+Passed
+Your regex should match the string FreeCodecamp
+Passed
+Your regex should not match the string Free Code Camp
+Passed
+Your regex should match the string FreeCOdeCamp
+Passed
+Your regex should not match the string FCC
+Passed
+Your regex should match the string FrEeCoDeCamp
+Passed
+Your regex should match the string FrEeCodECamp
+Passed
+Your regex should match the string FReeCodeCAmp
+*/
+
+// Extract Matches
