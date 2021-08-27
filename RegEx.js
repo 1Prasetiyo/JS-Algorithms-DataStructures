414fd419d95bf7135e797a13663f97e7c4e08222
Prasetiyo <prassonly@gmail.com>
2021-08-27 14:33:44 +0700

    Update RegEx.js

--- RegEx.js
+++ RegEx.js
@@ -540,3 +540,95 @@ Your regex should find 17 non-digits in the string 2001: A Space Odyssey.
 */
 
 /* Restrict Possible Usernames
+
+Usernames are used everywhere on the internet. They are what give users a unique identity on their favorite sites.
+
+You need to check all the usernames in a database. Here are some simple rules that users have to follow when creating their username.
+
+    Usernames can only use alpha-numeric characters.
+
+    The only numbers in the username have to be at the end. There can be zero or more of them at the end. Username cannot start with the number.
+
+    Username letters can be lowercase and uppercase.
+
+    Usernames have to be at least two characters long. A two-character username can only use alphabet letters as characters.
+
+Change the regex userCheck to fit the constraints listed above.
+*/
+
+let username = "JackOfAllTrades";
+let userCheck = /change/; // Change this line
+let result = userCheck.test(username);
+// Turn to
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i;
+let result = userCheck.test(username);
+console.log(result)
+
+/* Code Explanation
+    ^ - start of input
+    [a-z] - first character is a letter
+    [a-z]+ - following characters are letters
+    \d*$ - input ends with 0 or more digits
+    | - or
+    ^[a-z] - first character is a letter
+    \d\d+ - following characters are 2 or more digits
+    $ - end of input
+*/
+// or
+let username = "JackOfAllTrades";
+const userCheck = /^[a-z]([0-9]{2,}|[a-z]+\d*)$/i;
+let result = userCheck.test(username);
+
+/* Code Explanation
+    ^ - start of input
+    [a-z] - first character is a letter
+    [0-9]{2,0} - ends with two or more numbers
+    | - or
+    [a-z]+ - has one or more letters next
+    \d* - and ends with zero or more numbers
+    $ - end of input
+    i - ignore case of input
+*/
+
+/* Should :
+Your regex should match the string JACK
+Passed
+
+Your regex should not match the string J
+Passed
+
+Your regex should match the string Jo
+Passed
+
+Your regex should match the string Oceans11
+Passed
+
+Your regex should match the string RegexGuru
+Passed
+
+Your regex should not match the string 007
+Passed
+
+Your regex should not match the string 9
+Passed
+
+Your regex should not match the string A1
+Passed
+
+Your regex should not match the string BadUs3rnam3
+Passed
+
+Your regex should match the string Z97
+Passed
+
+Your regex should not match the string c57bT3
+Passed
+
+Your regex should match the string AB1
+Passed
+
+Your regex should not match the string J%4
+*/
+
+/* Match Whitespace
