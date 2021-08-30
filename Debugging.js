921f6fc060f758178cdbc5920f00e186f69fdb6c
Prasetiyo <prassonly@gmail.com>
2021-08-30 08:45:41 +0700

    Update Debugging.js

--- Debugging.js
+++ Debugging.js
@@ -46,3 +46,20 @@ First, use console.log to log the output variable. Then, use console.clear to cl
 */
 
 // Open your browser console.
+let output = "Get this to log once in the freeCodeCamp console and twice in the browser console";
+// Use console.log() to print the output variable.
+console.log(output);
+// Run the tests to see the difference between the two consoles.
+console.clear();
+// Now, add console.clear() before your console.log() to clear the browser console, and pass the tests.
+
+/* Should :
+Passed
+
+You should use console.clear() to clear the browser console.
+Passed
+
+You should use console.log() to print the output variable.
+*/
+
+/* Use typeof to Check the Type of a Variable
