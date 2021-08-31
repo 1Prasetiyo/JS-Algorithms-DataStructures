db794a46bc0defb7d706a2e2809b9a0f968ccb03
Prasetiyo <prassonly@gmail.com>
2021-08-31 09:50:13 +0700

    Update Debugging.js

--- Debugging.js
+++ Debugging.js
@@ -249,3 +249,45 @@ The condition should use either == or === to test for equality.
 */
 
 /* Catch Missing Open and Closing Parenthesis After a Function Call
+
+When a function or method doesn't take any arguments, you may forget to include the (empty) opening and closing parentheses when calling it. Often times the result of a function call is saved in a variable for other use in your code. This error can be detected by logging variable values (or their types) to the console and seeing that one is set to a function reference, instead of the expected value the function returns.
+
+The variables in the following example are different:
+
+function myFunction() {
+  return "You rock!";
+}
+let varOne = myFunction;
+let varTwo = myFunction();
+
+Here varOne is the function myFunction, and varTwo is the string You rock!.
+
+Fix the code so the variable result is set to the value returned from calling the function getNine.
+*/
+
+function getNine() {
+    let x = 6;
+    let y = 3;
+    return x + y;
+  }
+  
+  let result = getNine;
+  console.log(result);
+// Turn to
+function getNine() {
+    let x = 6;
+    let y = 3;
+    return x + y;
+  }
+  
+  let result = getNine();
+  console.log(result);
+
+/* Should :
+Your code should fix the variable result so it is set to the number that the function getNine returns.
+Passed
+
+Your code should call the getNine function.
+*/
+
+/* Catch Arguments Passed in the Wrong Order When Calling a Function
