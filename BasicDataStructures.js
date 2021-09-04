21c0913026d2b71ce6203cd78995442c2cf90763
Prasetiyo <prassonly@gmail.com>
2021-09-04 15:18:06 +0700

    Update BasicDataStructures.js

--- BasicDataStructures.js
+++ BasicDataStructures.js
@@ -902,3 +902,91 @@ The oranges, plums, and strawberries keys should be removed using delete.
 END*/
 
 /* Check if an Object has a Property
+
+Now we can add, modify, and remove keys from objects. But what if we just wanted to know if an object has a specific property? JavaScript provides us with two different ways to do this. One uses the hasOwnProperty() method and the other uses the in keyword. If we have an object users with a property of Alan, we could check for its presence in either of the following ways:
+
+users.hasOwnProperty('Alan');
+'Alan' in users;
+
+Both of these would return true.
+
+Finish writing the function so that it returns true if the object passed to it contains all four names, Alan, Jeff, Sarah and Ryan and returns false otherwise.
+*/
+
+let users = {
+    Alan: {
+      age: 27,
+      online: true
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: true
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+  };
+  
+  function isEveryoneHere(userObj) {
+    // Only change code below this line
+    
+    // Only change code above this line
+  }
+  
+  console.log(isEveryoneHere(users));
+// Turn to
+let users = {
+    Alan: {
+      age: 27,
+      online: true
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: true
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+  };
+  
+  function isEveryoneHere(userObj) {
+    // Only change code below this line
+    return ["Alan", "Jeff", "Sarah", "Ryan"].every(name => userObj.hasOwnProperty(name));
+    // Only change code above this line
+  }
+  
+  console.log(isEveryoneHere(users));
+
+/* Should :
+The users object should not be accessed directly
+Passed
+
+The users object should only contain the keys Alan, Jeff, Sarah, and Ryan
+Passed
+
+The function isEveryoneHere should return true if Alan, Jeff, Sarah, and Ryan are properties on the object passed to it.
+Passed
+
+The function isEveryoneHere should return false if Alan is not a property on the object passed to it.
+Passed
+
+The function isEveryoneHere should return false if Jeff is not a property on the object passed to it.
+Passed
+
+The function isEveryoneHere should return false if Sarah is not a property on the object passed to it.
+Passed
+
+The function isEveryoneHere should return false if Ryan is not a property on the object passed to it.
+END*/
+
+/* Iterate Through the Keys of an Object with a for...in Statement
