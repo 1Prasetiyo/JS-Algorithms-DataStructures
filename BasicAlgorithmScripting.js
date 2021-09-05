825eea52ee1f10f45d14c1347f94a7fc5301bccf
Prasetiyo <prassonly@gmail.com>
2021-09-05 19:04:49 +0700

    Update BasicAlgorithmScripting.js

--- BasicAlgorithmScripting.js
+++ BasicAlgorithmScripting.js
@@ -39,3 +39,45 @@ convertToF(30) should return a value of 86
 END*/
 
 /* Reverse a String
+
+Reverse the provided string.
+
+You may need to turn the string into an array before you can reverse it.
+
+Your result must be a string.
+*/
+
+function reverseString(str) {
+    return str;
+  }
+  
+  reverseString("hello");
+// Turn to
+function reverseString(str) {
+    return str
+      .split("")
+      .reverse()
+      .join("");
+  }
+// Or
+function reverseString(str) {
+    for (var reversedStr = "", i = str.length - 1; i >= 0; i--) {
+      reversedStr += str[i];
+    }
+    return reversedStr;
+  }
+
+/* Should :
+reverseString("hello") should return a string.
+Passed
+
+reverseString("hello") should return the string olleh.
+Passed
+
+reverseString("Howdy") should return the string ydwoH.
+Passed
+
+reverseString("Greetings from Earth") should return the string htraE morf sgniteerG.
+END*/
+
+/* Factorialize a Number
