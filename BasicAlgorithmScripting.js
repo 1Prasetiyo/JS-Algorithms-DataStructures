ab4deecd3624e401a9f9bece7a18b5ce0ab68be9
Prasetiyo <prassonly@gmail.com>
2021-09-06 16:27:51 +0700

    Update BasicAlgorithmScripting.js

--- BasicAlgorithmScripting.js
+++ BasicAlgorithmScripting.js
@@ -335,3 +335,101 @@ largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -1
 END*/
 
 /* Confirm the Ending
+
+Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+*/
+
+function confirmEnding(str, target) {
+  return str;
+}
+
+confirmEnding("Bastian", "n");
+// Turn to
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+
+  return str.slice(str.length - target.length) === target;
+}
+
+confirmEnding("He has to give me a new name", "name");
+
+/* Code Explanation
+
+    First we use the slice method copy the string.
+    In order to get the last characters in str equivalent to the target's length we use the slice method.
+    The first parameter inside the slice method is the starting index and the second parameter would be the ending index.
+    For example str.slice(10, 17) would return give me.
+    In this case we only include one parameter which it will copy everything from the starting index.
+    We substract the length of str and the length of target, that way, we shall get the last remaining characters equivalent to the target's length.
+    Finally we compare the return result of slice to target and check if they have the same characters.
+*/
+// Or
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+
+  let re = new RegExp(target + "$", "i");
+
+  return re.test(str);
+}
+
+console.log(confirmEnding("Bastian", "n"));
+
+/* Code Explanation
+
+    We need to make a pattern from the target variable that exists at the end of the string str.
+    Since we will use a variable that will change the pattern each time the function is called, we will use the constructor of the regular expression object new RegExp(pattern[, flags]), so we start with: new RegExp(target).
+    Then we have to check at the end of the string, so we concatenate to the target variable the $ character to match the end: new RegExp(target+'$').
+    We use the flag i to ignore the case of the pattern and we have our completed RegExp: new RegExp(target+'$','i'), or we can ommit the flag entirely.
+    Finally, we are using our regular expression with the test method to the given string, to check if the string ends with the pattern and return true or false accordingly.
+*/
+// Or
+function confirmEnding(str, target) {
+  return str.slice(-target.length) === target
+}
+
+confirmEnding("Bastian", "n");
+
+/* Code Explanation
+
+    If a negative number is provided as the first parameter to slice() , the offset is taken backwards from the end of the string.
+*/
+
+/* Should :
+confirmEnding("Bastian", "n") should return true.
+Passed
+
+confirmEnding("Congratulation", "on") should return true.
+Passed
+
+confirmEnding("Connor", "n") should return false.
+Passed
+
+confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification") should return false.
+Passed
+
+confirmEnding("He has to give me a new name", "name") should return true.
+Passed
+
+confirmEnding("Open sesame", "same") should return true.
+Passed
+
+confirmEnding("Open sesame", "sage") should return false.
+Passed
+
+confirmEnding("Open sesame", "game") should return false.
+Passed
+
+confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain") should return false.
+Passed
+
+confirmEnding("Abstraction", "action") should return true.
+Passed
+
+Your code should not use the built-in method .endsWith() to solve the challenge.
+END*/
+
+/* Repeat a String Repeat a String
