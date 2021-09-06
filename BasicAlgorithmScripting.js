db9cf8776bc08d9e87bdaa18fdd1a3c92e420888
Prasetiyo <prassonly@gmail.com>
2021-09-06 18:44:19 +0700

    Update BasicAlgorithmScripting.js

--- BasicAlgorithmScripting.js
+++ BasicAlgorithmScripting.js
@@ -433,3 +433,77 @@ Your code should not use the built-in method .endsWith() to solve the challenge.
 END*/
 
 /* Repeat a String Repeat a String
+
+Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
+*/
+
+function repeatStringNumTimes(str, num) {
+  return str.repeat(str);
+}
+
+repeatStringNumTimes("abc", 3);
+// Turn to
+function repeatStringNumTimes(str, num) {
+  var accumulatedStr = "";
+
+  while (num > 0) {
+    accumulatedStr += str;
+    num--;
+  }
+
+  return accumulatedStr;
+}
+
+/* Code Explanation
+
+    Create an empty string variable to store the repeated word.
+    Use a while loop or for loop to repeat code as many times as needed according to num
+    Then we just have to add the string to the variable created on step one, and increase or decrease num depending on how you set the loop.
+    At the end of the loop, return the variable for the repeated word.
+*/
+// Or
+function repeatStringNumTimes(str, num) {
+  if (num < 1) {
+    return "";
+  } else {
+    return str + repeatStringNumTimes(str, num - 1);
+  }
+}
+
+/* Code Explanation
+
+    This solution uses recursion.
+    We check if num is negative and return an empty string if true.
+    If not, we add the string to a call of our function with num being decreased by 1, which will add another str and another… until eventually num is 1. And return that whole process.
+*/
+// Or
+function repeatStringNumTimes(str, num) {
+  return num > 0 ? str + repeatStringNumTimes(str, num - 1) : '';
+}
+
+/* Should :
+repeatStringNumTimes("*", 3) should return the string ***.
+Passed
+
+repeatStringNumTimes("abc", 3) should return the string abcabcabc.
+Passed
+
+repeatStringNumTimes("abc", 4) should return the string abcabcabcabc.
+Passed
+
+repeatStringNumTimes("abc", 1) should return the string abc.
+Passed
+
+repeatStringNumTimes("*", 8) should return the string ********.
+Passed
+
+repeatStringNumTimes("abc", -2) should return an empty string ("").
+Passed
+
+The built-in repeat() method should not be used.
+Passed
+
+repeatStringNumTimes("abc", 0) should return "".
+END*/
+
+/* Truncate a String
