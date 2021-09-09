d306f2e5f67409619c6254b5cbc12bc9794451cf
Prasetiyo <prassonly@gmail.com>
2021-09-09 11:34:13 +0700

    Update BasicAlgorithmScripting.js

--- BasicAlgorithmScripting.js
+++ BasicAlgorithmScripting.js
@@ -763,3 +763,76 @@ titleCase("HERE IS MY HANDLE HERE IS MY SPOUT") should return the string Here Is
 END*/
 
 /* Slice and Splice
+
+You are given two arrays and an index.
+
+Copy each element of the first array into the second array, in order.
+
+Begin inserting elements at index n of the second array.
+
+Return the resulting array. The input arrays should remain the same after the function runs.
+*/
+
+function frankenSplice(arr1, arr2, n) {
+  return arr2;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
+// Turn to
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  let localArray = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    localArray.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return localArray;
+}
+
+/* Code Explanation
+
+    Our goal is to take all of the elements from arr1 and insert them into arr2 starting with index position n. At the same time we must ensurethat neither arr or arr2 have been mutated.
+
+    Using the slice() function we can create an exact replica of arr2 and assign the result of the operation to a variable, localArray.
+
+    Now that we have an array that we can mutate on, we can iterate through every item in the first array. For each item in the first array we can use the splice() function to insert the item into index n of localArray.
+
+    We increment the index n by one. This will ensure that every item from the arr1 is inserted into localArray in the proper index position.
+
+    Finally, we return the localArray and end the function.
+*/
+// Or
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  let localArr = arr2.slice();
+  localArr.splice(n, 0, ...arr1);
+  return localArr;
+}
+/*
+    Since our goal is to return the new array with out altering arr1 or arr2 we create a localArr and add all the items from arr2 using the slice() function
+
+    Since the splice() function will mutate (alter) arrays and can be used to add new elements we will use it to add the contents of arr1 into localArr. n is the starting position where our content will be inserted. We won’t be deleting any elements so the next argument is 0. Then we add the entire contents of arr1 using spread syntax ....
+
+    localArr is returned and the function is complete.
+*/
+
+/* Should :
+frankenSplice([1, 2, 3], [4, 5], 1) should return [4, 1, 2, 3, 5].
+Passed
+
+frankenSplice([1, 2], ["a", "b"], 1) should return ["a", 1, 2, "b"].
+Passed
+
+frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2) should return ["head", "shoulders", "claw", "tentacle", "knees", "toes"].
+Passed
+
+All elements from the first array should be added to the second array in their original order.
+Passed
+
+The first array should remain the same after the function runs.
+Passed
+
+The second array should remain the same after the function runs.
+END*/
+
+/* Falsy Bouncer
