c0e0497f2ee2e6c0d235ec212468345c265217f5
Prasetiyo <prassonly@gmail.com>
2021-09-10 22:53:31 +0700

    Update BasicAlgorithmScripting.js

--- BasicAlgorithmScripting.js
+++ BasicAlgorithmScripting.js
@@ -1038,3 +1038,113 @@ getIndexToIns([], 1) should return a number.
 END*/
 
 /* Mutations
+
+Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+
+Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+*/
+
+function mutation(arr) {
+  return arr;
+}
+
+mutation(["hello", "hey"]);
+// Solution 1
+/* Hints
+Hint 1
+
+    If everything is lowercase it will be easier to compare.
+
+Hint 2
+
+    Our strings might be easier to work with if they were arrays of characters.
+
+Hint 3
+
+    A loop might help. Use indexOf() to check if the letter of the second word is on the first.
+*/
+function mutation(arr) {
+  let test = arr[1].toLowerCase();
+  let target = arr[0].toLowerCase();
+  for (let i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0) return false;
+  }
+  return true;
+}
+
+/* Code Explanation
+
+First we make the two strings in the array lowercase. test will hold what we are looking for in target.
+Then we loop through our test characters and if any of them is not found we return false.
+
+If they are all found, the loop will finish without returning anything and we get to return true.
+*/
+// Solution 2
+function mutation(arr) {
+  return arr[1]
+    .toLowerCase()
+    .split("")
+    .every(function(letter) {
+      return arr[0].toLowerCase().indexOf(letter) != -1;
+    });
+}
+
+/* Code Explanation
+
+Grab the second string, lowercase and turn it into an array; then make sure every one of its letters is a part of the lowercased first string.
+
+Every will basically give you letter by letter to compare, which we do by using indexOf on the first string. indexOf will give you -1 if the current letter is missing. We check that not to be the case, for if this happens even once every will be false.
+*/
+// Solution 3
+function mutation([ target, test ], i = 0) {
+  target = target.toLowerCase();
+  test = test.toLowerCase();
+  return i >= test.length
+    ? true
+    : !target.includes(test[i])
+      ? false
+      : mutation([ target, test ], i + 1);
+}
+
+/* Should :
+mutation(["hello", "hey"]) should return false.
+Passed
+
+mutation(["hello", "Hello"]) should return true.
+Passed
+
+mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]) should return true.
+Passed
+
+mutation(["Mary", "Army"]) should return true.
+Passed
+
+mutation(["Mary", "Aarmy"]) should return true.
+Passed
+
+mutation(["Alien", "line"]) should return true.
+Passed
+
+mutation(["floor", "for"]) should return true.
+Passed
+
+mutation(["hello", "neo"]) should return false.
+Passed
+
+mutation(["voodoo", "no"]) should return false.
+Passed
+
+mutation(["ate", "date"]) should return false.
+Passed
+
+mutation(["Tiger", "Zebra"]) should return false.
+Passed
+
+mutation(["Noel", "Ole"]) should return true.
+END*/
+
+/* Chunky Monkey
