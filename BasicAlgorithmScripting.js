183b3058fec6ce323c4c2e8a6f3857de6d875b81
Prasetiyo <prassonly@gmail.com>
2021-09-10 22:52:05 +0700

    Update BasicAlgorithmScripting.js

--- BasicAlgorithmScripting.js
+++ BasicAlgorithmScripting.js
@@ -901,3 +901,140 @@ bouncer([null, NaN, 1, 2, undefined]) should return [1, 2].
 END*/
 
 /* Where do I Belong
+
+Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+
+Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
+*/
+
+function getIndexToIns(arr, num) {
+  return num;
+}
+
+getIndexToIns([40, 60], 50);
+// Turn to
+/* Hints
+Hint 1
+
+The first thing to do is sort the array from lower to bigger, just to make the code easier. This is where sort comes in, it needs a callback function so you have to create it.
+Hint 2
+
+Once the array is sorted, then just check for the first number that is bigger and return the index.
+Hint 3
+
+If there is no index for that number then you will have to deal with that case too.
+*/
+
+function getIndexToIns(arr, num) {
+  arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= num)
+      return i;
+  }
+
+  return arr.length;
+}
+
+/* Code Explanation
+
+    First I sort the array using .sort(callbackFunction) to sort it by lowest to highest, from left to right.
+    Then I use a for loop to compare the items in the array starting from the smallest one. When an item on the array is greater than the number we are comparing against, then we return the index.
+*/
+// Or
+function getIndexToIns(arr, num) {
+  return arr.filter(val => num > val).length;
+}
+
+/* Code Explanation
+
+    Count the number of entries that are smaller than the new value num
+    The new value would be inserted after these values
+*/
+// Or
+function getIndexToIns(arr, num) {
+  // sort and find right index
+  let index = arr
+    .sort((curr, next) => curr - next)
+    .findIndex(currNum => num <= currNum);
+  // Returns index or total length of arr
+  return index === -1 ? arr.length : index;
+}
+
+getIndexToIns([40, 60], 500);
+
+/* Code Explanation
+
+    First sort the array in ascending order, this is currently done using array functions for minimal footprint.
+    Once the array it is sorted, we directly apply the .findIndex() where we are going to compare every element in the array until we find where num <= currNum meaning where the number we want to insert is less or equal to the current number number in the iteration.
+    Then we use ternary operations to check whether we got an index returned or -1. We only get -1 when the index was not found meaning when we get a false for all elements int he array, and for such case, it would mean that num should be inserted at the end of the list hence why we use arr.length.
+*/
+// Or
+function getIndexToIns(arr, num) {
+  return arr
+    .concat(num)
+    .sort((a, b) => a - b)
+    .indexOf(num);
+}
+
+getIndexToIns([1, 3, 4], 2);
+
+/* Code Explanation
+
+    We use method-chaining to invoke one method after another to solve the problem in a single line. First we create a new array with the contents of arr and num by using the concat() method
+    Then we use sort() with the callback arrow function (a, b) => return a-b to sort the numbers in ascending order
+    Lastly we return the position or index of num in the array with the indexOf() method
+*/
+
+/* Should :
+getIndexToIns([10, 20, 30, 40, 50], 35) should return 3.
+Passed
+
+getIndexToIns([10, 20, 30, 40, 50], 35) should return a number.
+Passed
+
+getIndexToIns([10, 20, 30, 40, 50], 30) should return 2.
+Passed
+
+getIndexToIns([10, 20, 30, 40, 50], 30) should return a number.
+Passed
+
+getIndexToIns([40, 60], 50) should return 1.
+Passed
+
+getIndexToIns([40, 60], 50) should return a number.
+Passed
+
+getIndexToIns([3, 10, 5], 3) should return 0.
+Passed
+
+getIndexToIns([3, 10, 5], 3) should return a number.
+Passed
+
+getIndexToIns([5, 3, 20, 3], 5) should return 2.
+Passed
+
+getIndexToIns([5, 3, 20, 3], 5) should return a number.
+Passed
+
+getIndexToIns([2, 20, 10], 19) should return 2.
+Passed
+
+getIndexToIns([2, 20, 10], 19) should return a number.
+Passed
+
+getIndexToIns([2, 5, 10], 15) should return 3.
+Passed
+
+getIndexToIns([2, 5, 10], 15) should return a number.
+Passed
+
+getIndexToIns([], 1) should return 0.
+Passed
+
+getIndexToIns([], 1) should return a number.
+END*/
+
+/* Mutations
