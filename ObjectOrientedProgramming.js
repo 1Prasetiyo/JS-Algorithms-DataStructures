6f5cba77770410f64bf01356730a2cd0730f8d85
Prasetiyo <prassonly@gmail.com>
2021-09-11 16:07:45 +0700

    Update ObjectOrientedProgramming.js

--- ObjectOrientedProgramming.js
+++ ObjectOrientedProgramming.js
@@ -1,1 +1,46 @@
+/* Create a Basic JavaScript Object
 
+Think about things people see every day, like cars, shops, and birds. These are all objects: tangible things people can observe and interact with.
+
+What are some qualities of these objects? A car has wheels. Shops sell items. Birds have wings.
+
+These qualities, or properties, define what makes up an object. Note that similar objects share the same properties, but may have different values for those properties. For example, all cars have wheels, but not all cars have the same number of wheels.
+
+Objects in JavaScript are used to model real-world objects, giving them properties and behavior just like their real-world counterparts. Here's an example using these concepts to create a duck object:
+
+let duck = {
+  name: "Aflac",
+  numLegs: 2
+};
+
+This duck object has two property/value pairs: a name of Aflac and a numLegs of 2.
+
+Create a dog object with name and numLegs properties, and set them to a string and a number, respectively.
+*/
+
+let dog = {
+
+};
+
+/* Problem Explanation
+
+The soultion to this problem is more or less identical to the example given.
+Give the dog object two new properties - name and numLegs - and set them to a string and a number, respectively.
+*/
+// SOLUTION
+let dog = {
+    name: "George",
+    numLegs: 4
+  };
+
+/* Should :
+dog should be an object.
+Passed
+
+dog should have a name property set to a string.
+Passed
+
+dog should have a numLegs property set to a number.
+END*/
+
+/* Use Dot Notation to Access the Properties of an Object
