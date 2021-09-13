0ff00ee964845810f5dd5ffa1c03e4e70482407c
Prasetiyo <prassonly@gmail.com>
2021-09-13 11:33:53 +0700

    Update ObjectOrientedProgramming.js

--- ObjectOrientedProgramming.js
+++ ObjectOrientedProgramming.js
@@ -44,3 +44,32 @@ dog should have a numLegs property set to a number.
 END*/
 
 /* Use Dot Notation to Access the Properties of an Object
+
+The last challenge created an object with various properties. Now you'll see how to access the values of those properties. Here's an example:
+
+let duck = {
+  name: "Aflac",
+  numLegs: 2
+};
+console.log(duck.name);
+
+Dot notation is used on the object name, duck, followed by the name of the property, name, to access the value of Aflac.
+
+Print both properties of the dog object to your console.
+*/
+
+let dog = {
+    name: "Spot",
+    numLegs: 4
+  };
+  console.log(dog.name);
+  console.log(dog.numLegs);
+
+/* Should :
+Your code should use console.log to print the value for the name property of the dog object.
+Passed
+
+Your code should use console.log to print the value for the numLegs property of the dog object.
+END*/
+
+/* Create a Method on an Object
