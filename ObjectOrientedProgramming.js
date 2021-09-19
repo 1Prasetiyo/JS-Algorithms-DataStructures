1becb4fa5995cca51b650007461a8516ba546d6c
Prasetiyo <prassonly@gmail.com>
2021-09-19 17:05:21 +0700

    Update ObjectOrientedProgramming.js

--- ObjectOrientedProgramming.js
+++ ObjectOrientedProgramming.js
@@ -728,3 +728,42 @@ Dog.prototype should set the constructor property.
 END*/
 
 /* Understand Where an Object’s Prototype Comes From
+
+Just like people inherit genes from their parents, an object inherits its prototype directly from the constructor function that created it. For example, here the Bird constructor creates the duck object:
+
+function Bird(name) {
+  this.name = name;
+}
+
+let duck = new Bird("Donald");
+
+duck inherits its prototype from the Bird constructor function. You can show this relationship with the isPrototypeOf method:
+
+Bird.prototype.isPrototypeOf(duck);
+
+This would return true.
+
+Use isPrototypeOf to check the prototype of beagle.
+*/
+
+function Dog(name) {
+    this.name = name;
+  }
+  
+  let beagle = new Dog("Snoopy");
+  
+  // Only change code below this line
+// SOLUTION
+function Dog(name) {
+    this.name = name;
+  }
+  
+let beagle = new Dog("Snoopy");
+  
+Dog.prototype.isPrototypeOf(beagle);
+
+/* Should :
+You should show that Dog.prototype is the prototype of beagle
+END*/
+
+/* Understand the Prototype Chain
