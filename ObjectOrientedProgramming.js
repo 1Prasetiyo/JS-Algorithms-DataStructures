87a920f0f201ae6c70a27ccc9e56e138ed1a38a5
prasetiyojs <prassonly@gmail.com>
2021-09-21 12:16:53 +0700

    update

--- ObjectOrientedProgramming.js
+++ ObjectOrientedProgramming.js
@@ -1388,3 +1388,35 @@ Passed
 
 Your getWeight function should return the private variable weight.
 END*/
+
+/* Understand the Immediately Invoked Function Expression (IIFE)
+
+A common pattern in JavaScript is to execute a function as soon as it is declared:
+
+(function () {
+  console.log("Chirp, chirp!");
+})();
+
+This is an anonymous function expression that executes right away, and outputs Chirp, chirp! immediately.
+
+Note that the function has no name and is not stored in a variable. The two parentheses () at the end of the function expression cause it to be immediately executed or invoked. This pattern is known as an immediately invoked function expression or IIFE.
+
+Rewrite the function makeNest and remove its call so instead it's an anonymous immediately invoked function expression (IIFE).
+*/
+
+function makeNest() {
+  console.log("A cozy nest is ready");
+}
+
+makeNest();
+// SOLUTION
+(function () {
+  console.log("A cozy nest is ready");
+})();
+
+/* Should :
+The function should be anonymous.
+Passed
+
+Your function should have parentheses at the end of the expression to call it immediately.
+END*/
