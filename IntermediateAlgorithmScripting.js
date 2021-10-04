3fceabf72d5b7cc2da1e1601db81f3c56b95195d
prasetiyojs <prassonly@gmail.com>
2021-10-04 18:24:23 +0700

    update

--- IntermediateAlgorithmScripting.js
+++ IntermediateAlgorithmScripting.js
@@ -1345,3 +1345,151 @@ Passed
 
 uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].
 END*/
+
+/* Convert HTML Entities
+
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+*/
+
+function convertHTML(str) {
+  return str;
+}
+
+convertHTML("Dolce & Gabbana");
+// SOLUTION
+/* Problem Explanation
+
+    You have to create a program that will convert HTML entities from string to their corresponding HTML entities. There are only a few so you can use different methods.
+
+Hints
+Hint 1
+
+    You can use regular Expressions however I didn’t in this case.
+
+Hint 2
+
+    You will benefit from a chart with all the html entities so you know which ones are the right ones to put.
+
+Hint 3
+
+    You should separate the string and work with each character to convert the right ones and then join everything back up.
+*/
+
+function convertHTML(str) {
+  // Split by character to avoid problems.
+
+  var temp = str.split("");
+
+  // Since we are only checking for a few HTML elements, use a switch
+
+  for (var i = 0; i < temp.length; i++) {
+    switch (temp[i]) {
+      case "<":
+        temp[i] = "&lt;";
+        break;
+      case "&":
+        temp[i] = "&amp;";
+        break;
+      case ">":
+        temp[i] = "&gt;";
+        break;
+      case '"':
+        temp[i] = "&quot;";
+        break;
+      case "'":
+        temp[i] = "&apos;";
+        break;
+    }
+  }
+
+  temp = temp.join("");
+  return temp;
+}
+
+//test here
+convertHTML("Dolce & Gabbana");
+
+/* Code Explanation
+
+    Assign temp to str.split(''), which creates an array containing each individual character in the passed in string.
+    Pass each character in the newly created array into a switch() statement.
+    Replace the HTML entities with their corresponding HTML entity string (i.e. '&' becomes '&amp;' in line 51)
+    temp.join('') converts the array of characters into a string to be returned.
+*/
+
+// SOLUTION 2
+function convertHTML(str) {
+  // Use Object Lookup to declare as many HTML entities as needed.
+  const htmlEntities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;"
+  };
+  // Using a regex, replace characters with it's corresponding html entity
+  return str.replace(/([&<>\"'])/g, match => htmlEntities[match]);
+}
+
+// test here
+convertHTML("Dolce & Gabbana");
+
+/* Code Explanation
+
+    Create an object to use the Lookup functionality and easily find the characters.
+    Use replace() to replace characters with regex.
+    The first argument for replace() is a regex that catches all the target characters and puts them into a capturing group.
+    The second arguments for replace() is a function with the matched character as a parameter. It returns the correspondant entity from htmlEntities.
+*/
+
+// SOLUTION 3
+function convertHTML(str) {
+  // Use Object Lookup to declare as many HTML entities as needed.
+  const htmlEntities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;"
+  };
+  //Use map function to return a filtered str with all entities changed automatically.
+  return str
+    .split("")
+    .map(entity => htmlEntities[entity] || entity)
+    .join("");
+}
+
+// test here
+convertHTML("Dolce & Gabbana");
+
+/* Code Explanation
+
+    Create an object to use the Lookup functionality and easily find the characters.
+    Split the original string by characters and use map to check for the changed html entity or use the same one.
+    The a function is added which is what returns the converted entity or the original one if there is no conversion.
+    Lastly we join all the characters once again.
+
+Note that if you went the regex route then you don’t need to join anything, just make sure you return the whole operation or save it to a variable and then return it.
+*/
+
+/* Should :
+convertHTML("Dolce & Gabbana") should return the string Dolce &amp; Gabbana.
+Passed
+
+convertHTML("Hamburgers < Pizza < Tacos") should return the string Hamburgers &lt; Pizza &lt; Tacos.
+Passed
+
+convertHTML("Sixty > twelve") should return the string Sixty &gt; twelve.
+Passed
+
+convertHTML('Stuff in "quotation marks"') should return the string Stuff in &quot;quotation marks&quot;.
+Passed
+
+convertHTML("Schindler's List") should return the string Schindler&apos;s List.
+Passed
+
+convertHTML("<>") should return the string &lt;&gt;.
+Passed
+
+convertHTML("abc") should return the string abc.
+END*/
