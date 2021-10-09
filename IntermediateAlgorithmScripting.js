7584980be2feb2aff3338cf9c865644c31a0f5a1
prasetiyojs <prassonly@gmail.com>
2021-10-09 10:16:17 +0700

    update

--- IntermediateAlgorithmScripting.js
+++ IntermediateAlgorithmScripting.js
@@ -2293,3 +2293,148 @@ Passed
 
 binaryAgent("01001001 00100000 01101100 01101111 01110110 01100101 00100000 01000110 01110010 01100101 01100101 01000011 01101111 01100100 01100101 01000011 01100001 01101101 01110000 00100001") should return the string I love FreeCodeCamp!
 END*/
+
+/* Everything Be True
+
+Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+In other words, you are given an array collection of objects. The predicate pre will be an object property and you need to return true if its value is truthy. Otherwise, return false.
+
+In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context.
+
+Remember, you can access object properties through either dot notation or [] notation.
+*/
+
+function truthCheck(collection, pre) {
+  return pre;
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+// SOLUTION
+/* Problem Explanation
+
+The program needs to check if the second argument is a truthy 2.7k element, and it must check this for each object in the first argument.
+
+Hints
+Hint 1
+
+Remember to iterate through the first argument to check each object.
+Hint 2
+
+Only if all of them are truthy will we return true, so make sure all of them check.
+Hint 3
+
+You could use loops or callback functions, there are multiple ways to solve this problem.
+*/
+
+// Using for-in loop & hasOwnProperty
+
+function truthCheck(collection, pre) {
+  // Create a counter to check how many are true.
+  var counter = 0;
+  // Check for each object
+  for (var c in collection) {
+    // If it is has property and value is truthy
+    if (collection[c].hasOwnProperty(pre) && Boolean(collection[c][pre])) {
+      counter++;
+    }
+  }
+  // Outside the loop, check to see if we got true for all of them and return true or false
+  return counter == collection.length;
+}
+
+// test here
+truthCheck(
+  [
+    { user: "Tinky-Winky", sex: "male" },
+    { user: "Dipsy", sex: "male" },
+    { user: "Laa-Laa", sex: "female" },
+    { user: "Po", sex: "female" }
+  ],
+  "sex"
+);
+
+/* Code Explanation
+
+    First I create a counter to check how many cases are actually true.
+    Then check for each object if the value is truthy
+    Outside the loop, I check to see if the counter variable has the same value as the length of collection, if true then return true, otherwise, return false
+*/
+
+
+// SOLUTION - Using Array.every()
+
+function truthCheck(collection, pre) {
+  return collection.every(function(element) {
+    return element.hasOwnProperty(pre) && Boolean(element[pre]);
+  });
+}
+
+// test here
+truthCheck(
+  [
+    { user: "Tinky-Winky", sex: "male" },
+    { user: "Dipsy", sex: "male" },
+    { user: "Laa-Laa", sex: "female" },
+    { user: "Po", sex: "female" }
+  ],
+  "sex"
+);
+
+/* Code Explanation
+
+    Uses the native “every” method to test whether all elements in the array pass the test.
+    This link will help Array.prototype.every() 
+*/
+
+// SOLUTION 3
+
+function truthCheck(collection, pre) {
+  // Is everyone being true?
+  return collection.every(obj => obj[pre]);
+}
+
+truthCheck(
+  [
+    { user: "Tinky-Winky", sex: "male" },
+    { user: "Dipsy", sex: "male" },
+    { user: "Laa-Laa", sex: "female" },
+    { user: "Po", sex: "female" }
+  ],
+  "sex"
+);
+
+/* Code Explanation
+
+    For every object in the collection array, check the truthiness of object’s property passed in pre parameter
+    Array.prototype.every method internally checks if the value returned from the callback is truthy.
+    Return true if it passes for every object. Otherwise, return false.
+*/
+
+/* Should :
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") should return true.
+Passed
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") should return false.
+Passed
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age") should return false.
+Passed
+
+truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastForward", "onBoat": null}], "onBoat") should return false.
+Passed
+
+truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastForward", "onBoat": true}], "onBoat") should return true.
+Passed
+
+truthCheck([{"single": "yes"}], "single") should return true.
+Passed
+
+truthCheck([{"single": ""}, {"single": "double"}], "single") should return false.
+Passed
+
+truthCheck([{"single": "double"}, {"single": undefined}], "single") should return false.
+Passed
+
+truthCheck([{"single": "double"}, {"single": NaN}], "single") should return false.
+END*/
